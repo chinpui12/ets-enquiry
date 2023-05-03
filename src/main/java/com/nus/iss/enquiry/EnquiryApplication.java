e738bbf1e721c85614aa1be133936b47a1e6e105
CP <chinpui.chai@gmail.com>
2023-05-03 23:08:22 +0800

    fix dependency error

--- src/main/java/com/nus/iss/enquiry/EnquiryApplication.java
+++ src/main/java/com/nus/iss/enquiry/EnquiryApplication.java
@@ -2,8 +2,10 @@ package com.nus.iss.enquiry;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.nus.iss.*")
 public class EnquiryApplication {
 
 	public static void main(String[] args) {
